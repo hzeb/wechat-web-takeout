7c798b6fae2d21e4edf6fd1e42cff877a3949588
hzeb <1042220187@qq.com>
2016-11-02 23:20:23 +0800

    商品页面样式调整

--- pages/components/dishes/dishes.js
+++ pages/components/dishes/dishes.js
@@ -27,50 +27,57 @@ Page({
 		dishesList:[
 			[
 				{
-					name:"红烧肉",
+					name:"鸡翅",
 					price:38,
 					num:1,
-					id:1
+					id:1,
+					img:'/Images/jichi.png'
 				},
 				{
-					name:"宫保鸡丁",
+					name:"生蚝",
 					price:58,
 					num:1,
-					id:29
+					id:29,
+					img:'/Images/shenghao.png'
 				},
 				{
-					name:"水煮鱼",
+					name:"热狗",
 					price:88,
 					num:1,
-					id:2
+					id:2,
+					img:'/Images/regou.png'
 				}
 			],
 			[
 				{
-					name:"小炒日本豆腐",
+					name:"小龙虾",
 					price:18,
 					num:1,
-					id:3
+					id:3,
+					img:'/Images/xiaolongxia.png'
 				},
 				{
 					name:"烤鱼",
 					price:58,
 					num:1,
-					id:4
+					id:4,
+					img:'/Images/shenghao.png'
 				}
 			],
 			[
 				{
-					name:"大拌菜",
+					name:"韭菜",
 					price:18,
 					num:1,
-					id:5
+					id:5,
+					img:'/Images/jiucai.png'
 				},
 				{
-					name:"川北凉粉",
+					name:"牛肉串",
 					price:8,
 					num:1,
-					id:6
+					id:6,
+					img:'/Images/niurouchuan.png'
 				}
 			],
 			[]
@@ -82,7 +89,7 @@ Page({
 			this.setData({
 				hidden:true
 			})
-		},2000)
+		},500)
 	},
 	selectNav (event) {
 		let id = event.target.dataset.id,
@@ -96,20 +103,25 @@ Page({
 	// 选择菜品
 	selectDish (event) {
 		let dish = event.currentTarget.dataset.dish;
+		let price = event.currentTarget.dataset.price*1;
 		let flag = true;
 		let	cart = this.data.cart;
-		
+		let total = this.data.cartTotal;
 		if(cart.length > 0){
 			cart.forEach(function(item,index){
 				if(item == dish){
 					cart.splice(index,1);
 					flag = false;
+					total -= price;
 				}
 			})
 		}
-		if(flag) cart.push(dish);
+		if(flag){
+			cart.push(dish);
+			total += price;
+		}
 		this.setData({
-			cartTotal:cart.length
+			cartTotal:total
 		})
 		this.setStatus(dish)
 	},
